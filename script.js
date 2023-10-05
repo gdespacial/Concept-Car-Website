cc66f381e005b321fffb1a0e9351e02e6d12d1a1
gdespacial <132879802+gdespacial@users.noreply.github.com>
2023-10-05 19:35:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    window.scrollTo(0, 0);
+    borrarDatos(); 
+}
+
+function borrarDatos() {
+    
+    document.getElementById('nombre').value = '';
+    document.getElementById('mensaje').value = '';
+
+   
+}
+
+
+
